35c7c8841f5e99d50c0d60bda5ed12f725409e56
AlexanderPlotnikovv <appplotnikov2019@gmail.com>
2023-08-30 14:07:04 +0300

    New commit

--- Homework Move Semantic N1/main.cpp
+++ Homework Move Semantic N1/main.cpp
@@ -4,16 +4,13 @@
 template<class T>
 void move_vectors(std::vector<T>& vector, std::vector<T>& vector_copy)
 {
-	for (int i = 0; i != vector_copy.size(); ++i)
-	{
-		vector[i] = std::move(vector_copy[i]);
-	}
+	vector = std::move(vector_copy);
 }
 
 int main()
 {
 	std::vector <std::string> one = { "test_string1", "test_string2" };
-	std::vector <std::string> two(one.size());
+	std::vector <std::string> two;
 	move_vectors(two, one);
 	for (auto elem : two)
 	{
